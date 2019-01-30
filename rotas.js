7bdbbb8a65ed8a1b7074a318eee6c4f0d658d7db
Willian Gabriel Cerqueira da Rocha <willianrocha@riseup.net>
2019-01-30 18:38:17 -0300

    Update rotas.js

--- rotas.js
+++ rotas.js
@@ -1,3 +1,24 @@
+/* 
+	Copyright © 2019 Willian Gabriel Cerqueira da Rocha
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	All rights reserved.
+
+	Willian Gabriel Cerqueira da Rocha
+	willianrocha[at]riseup[dot]net
+	http://willianrocha.tk
+*/
+
 document.addEventListener ("DOMContentLoaded", () => {
 	//Quantidade de cidades que serão geradas para otimização de rota
 	const quantidadeCidades = 30;
